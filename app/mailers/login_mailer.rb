6b428f09ac32b0e6c019ee964d708a61b968f59d
Simon Walsh <slfwalsh@gmail.com>
2016-10-05 12:25:25 +0100

    Added proc file for heroku

--- app/mailers/login_mailer.rb
+++
@@ -1,7 +0,0 @@
-class LoginMailer < ApplicationMailer
-  def notify_admin_login(user)
-    @email = user.email
-    @user = user
-    mail to: "slfwalsh@gmail.com", subject: "#{user.email} just logged in"
-  end
-end
